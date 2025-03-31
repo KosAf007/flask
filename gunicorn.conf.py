b6b11cd994b67e4f754f8fd7883866d0f7a387ad
Kos Af <afsnasiev1986@gmail.com>
2025-03-31 19:46:34 +0300

    Add Gunicorn config to output logs to stdout

---
+++ gunicorn.conf.py
@@ -0,0 +1,42 @@
+import sys
+
+# Налаштування логування Gunicorn
+logconfig_dict = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "format": "[%(asctime)s] [%(process)d] [%(levelname)s] %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S %z",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "level": "INFO",
+            "formatter": "default",
+            "stream": sys.stdout,  # Виводимо логи в stdout
+        },
+    },
+    "loggers": {
+        "gunicorn": {
+            "level": "INFO",
+            "handlers": ["console"],
+            "propagate": False,
+        },
+        "gunicorn.access": {
+            "level": "INFO",
+            "handlers": ["console"],
+            "propagate": False,
+        },
+        "gunicorn.error": {
+            "level": "INFO",
+            "handlers": ["console"],
+            "propagate": False,
+        },
+    },
+    "root": {
+        "level": "INFO",
+        "handlers": ["console"],
+    },
+}
